f71269491d3a3232cb4531fb6d6de09077c1de34
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-01-28 12:57:30 +0000

    Add Extended Euclid's algorithm script

---
+++ CIS/extEuclid.py
@@ -0,0 +1,66 @@
+
+import math
+from tabulate import tabulate
+
+# A function for computing x and y such that gcd(a,b) = (x*a) + (y*b),
+# using the Extended Euclid's algorithm.
+# Parameters:
+#   a, b - integers
+def extEuclid(a, b):
+  if b == 0:
+    print(a, b, "-", a, 1, 0)
+    return (a, 1, 0)
+  else:
+    (d1, x1, y1) = extEuclid(b, a % b)
+    q = math.floor(a / b)
+    print(d1, x1, y1)
+    (d, x, y) = (d1, y1, x1 - q * y1)
+    print(a, b, q, d, x, y)
+    return (d, x, y)
+
+# extEuclid(99, 78)
+
+# A function for computing x and y such that gcd(a,b) = (x*a) + (y*b),
+# using the Extended Euclid's algorithm, but with passed in list to store data.
+# Helper function for extEuclidTable.
+# Parameters:
+#   a, b - integers
+#   data - list of integers
+def extEuclidData(a, b, data):
+  if b == 0:
+    row = [a, b, "-", a, 1, 0]
+    data.append(row)
+    return (a, 1, 0)
+  else:
+    (d1, x1, y1) = extEuclidData(b, a % b, data)
+    q = math.floor(a / b)
+    (d, x, y) = (d1, y1, x1 - q * y1)
+    row = [a, b, q, d, x, y]
+    data.append(row)
+    return (d, x, y)
+
+# A function for computing x and y such that gcd(a,b) = (x*a) + (y*b),
+# using the Extended Euclid's algorithm.
+# Prints out a formatted table.
+# Parameters:
+#   a, b - integers
+def extEuclidTable(a, b):
+  data = [] 
+  headers = ["a", "b", "[a/b]", "d", "x", "y"]
+
+  extEuclidData(a,b, data)
+  data.reverse()
+
+  print()
+  print(tabulate(data, headers))
+  print()
+
+  print("For RSA")
+  y = data[0][5]
+  totient = a
+  if y < 0:
+    print(f"d = y mod a = {y % totient}")
+  else:
+    print(f"d = y = {y}")
+
+extEuclidTable(72,23)
